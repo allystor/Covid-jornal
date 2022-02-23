68931b2f99c6a9d1ee712423fd31354434123e6c
Álvaro Veiga <80602026+allystor@users.noreply.github.com>
2022-02-23 11:07:43 -0300

    Update commit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+
+from distutils.log import debug
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,38 +8,45 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/')
+def static_file():
+    return app.send_static_file('index.html')
+
 @app.route('/Noticias/1')
 def news1():
     return render_template('/Noticias/news-1.html')
 
-@app.route('/Notícias/news-2.html')
+@app.route('/Notícias/2')
 def news2():
-    return render_template('news-2.html')
+    return render_template('/Noticias/news-2.html')
 
-@app.route('/Notícias/news-3.html')
+@app.route('/Notícias/3')
 def news3():
-    return render_template('nes-3.html')
+    return render_template('/Noticias/news-3.html')
 
-@app.route('/Notícias/news-4.html')
+@app.route('/Notícias/4')
 def news4():
-    return render_template('news-4.html')
+    return render_template('/Noticias/news-4.html')
 
-@app.route('/Notícias/news-5.html')
+@app.route('/Notícias/5')
 def news5():
-    return render_template('news-5.html')
+    return render_template('/Noticias/news-5.html')
 
-@app.route('/Notícias/news-6.html')
+@app.route('/Notícias/6')
 def news6():
-    return render_template('news-6.html')
+    return render_template('/Noticias/news-6.html')
 
-@app.route('/Notícias/news-7.html')
+@app.route('/Notícias/7')
 def news7():
-    return render_template('news-7.html')
+    return render_template('/Noticias/news-7.html')
 
-@app.route('/Notícias/news-8.html')
+@app.route('/Notícias/8')
 def news8():
-    return render_template('news-8.html')
+    return render_template('/Noticias/news-8.html')
 
-@app.route('/Notícias/news-9.html')
+@app.route('/Notícias/9')
 def news9():
-    return render_template('news-9.html')
+    return render_template('/Noticias/news-9.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
