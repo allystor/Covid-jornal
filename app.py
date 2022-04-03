3fe7b25c3ec694db672cdfeed14e7ba428b76c50
allystor <alvarojoao17@gmail.com>
2022-04-02 23:50:25 -0300

    update commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 from distutils.log import debug
-from Bandeiras import Estado
-from Pagina import Pagina
+from bandeiras import Estado
+from pagina import Pagina
+from noticia import Noticia
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
@@ -17,41 +18,42 @@ listas_de_paginas_recentes = [
     Pagina(4,'Covid-19: De onde poderá vir a próxima variante? Estudo aponta países prováveis','/static/img/covid-news-5.jpg','O vírus que causa a Covid-19 foi sequenciado pela primeira vez no início de 2020 e desde então já foram identificadas pela Organização Mundial de Saúde cinco "variantes preocupantes". A mais recente, a Ômicron, foi descoberta em novembro e é agora dominante em quase todos os países do mundo – a nova variante pode evitar a imunidade induzida pela vacina e se espalha mais rapidamente do que a Delta. No entanto, uma subvariante, a BA.2, parece ser ainda mais transmissível. Esta é a história que já vivemos, agora vamos à história que nos reserva – é improvável que estas variantes sejam as últimas. É impossível ter a certeza de como o vírus vai evoluir mas pesquisadores da Airfinity, na Inglaterra, empresa de dados de ciências da vida, tentaram mapear o local mais provável de aparecimento da próxima variante. A mutação de um vírus é um processo aleatório e é por isso que novas variantes bem-sucedidas são mais propensas a vir de lugares onde muitas mutações estão ocorrendo. A hipótese da Airfinity é de que vai ocorrer num local onde poucas pessoas estarão vacinadas e onde muitos sofrem com sistemas imunológicos enfraquecidos. As pessoas imunocomprometidas tendem a abrigar o vírus por mais tempo porque os respetivos corpos lutam mais para o combater, dando mais tempo para que as mutações bem-sucedidas possam se acumular e são menos propensos a produzir anticorpos após serem vacinados, o que significa que têm menos proteção contra a reinfecção.'), 
     Pagina(5,'Ministério da Saúde decide não recomendar 4ª dose de vacina contra a Covid-19','/static/img/covid-news-6.jpg','Após reunião da Câmara Técnica de Assessoramento em Imunização da Covid-19 (CTAI), nesta sexta-feira (11), o Ministério da Saúde decidiu que não vai recomendar a quarta dose de vacina no Brasil. As informações foram antecipadas pela CNN ontem. Segundo fontes da câmara técnica, o entendimento é de que não há pesquisas científicas com dados suficientes que embasam um novo reforço de imunização para pessoas que não sejam imunossuprimidas. O comitê também analisou o cenário epidemiológico atual do país antes de chegar a esta conclusão. Ainda de acordo com técnicos que compõem o comitê técnico, neste momento não deve ser aplicada a quarta dose porque as pessoas que tomaram duas doses e o reforço, ou seja, estão com o esquema vacinal completo, continuam muito bem protegidas contra a Covid- 19 e o agravamento da doença. A pasta definiu o posicionamento depois que o estado de São Paulo ampliou a vacinação ao público em geral com idade acima de 60 anos, com previsão para começar a aplicar a quarta dose a partir do dia 4 de abril. Em entrevista à CNN, o coordenador-executivo do Comitê Científico do estado de São Paulo, João Gabbardo, analisou que há evidências e experiências de outros países que mostram um benefício da dose extra de vacina para os idosos.')  
 ]
-listas_de_paginas_noticias =[
-    Pagina(6,'Acre recebe mais 33,2 mil doses de vacina contra a Covid-19','https://agencia.ac.gov.br/wp-content/uploads/2021/09/033e59eb-ba83-4555-af4d-e13391dbff6c-1200x800.jpg','https://agencia.ac.gov.br/wp-content/uploads/2021/09/033e59eb-ba83-4555-af4d-e13391dbff6c-1200x800.jpg')
-]
+
 listas_de_paginas_estado =[
     Estado(0,'Acre','/static/img/bandeiras/Acre.png'),
     Estado(1,'Alagoas','/static/img/bandeiras/Alagoas.png'),
-    Estado(2,'Amapá','/static/img/bandeiras/Amapa.png'),
+    Estado(2,'Amapá','/static/img/bandeiras/Amapá.png'),
     Estado(3,'Amazonas','/static/img/bandeiras/Amazonas.png'),
     Estado(4,'Bahia','/static/img/bandeiras/Bahia.png'),
-    Estado(5,'Ceará','/static/img/bandeiras/Ceara.png'),
+    Estado(5,'Ceará','/static/img/bandeiras/Ceará.png'),
     Estado(6,'Distrito Federal','/static/img/bandeiras/distrito_federal.png'),
-    Estado(7,'Espírito Santo','/static/img/bandeiras/espirito_santo.png'),
-    Estado(8,'Goiás','/static/img/bandeiras/goias.png'),
-    Estado(9,'Maranhão','/static/img/bandeiras/maranhao.png'),
-    Estado(10,'Mato Grosso','/static/img/bandeiras/mato_grosso.png'),
-    Estado(11,'Mato Grosso do Sul','/static/img/bandeiras/mato_grosso_do_sul.png'),
-    Estado(12,'Minas Gerais','/static/img/bandeiras/minas_gerais.png'),
-    Estado(13,'Pará','/static/img/bandeiras/para.png'),
-    Estado(14,'Paraíba','/static/img/bandeiras/paraiba.png'),
-    Estado(15,'Paraná','/static/img/bandeiras/parana.png'),
-    Estado(16,'Pernambuco','/static/img/bandeiras/pernambuco.png'),
-    Estado(17,'Piauí','/static/img/bandeiras/piaui.png'),
-    Estado(18,'Rio de Janeiro','/static/img/bandeiras/rio_de_janeiro.png'),
-    Estado(19,'Rio Grande do Norte','/static/img/bandeiras/rio_grande_do_norte.png'),
-    Estado(20,'Rio Grande do Sul','/static/img/bandeiras/rio_grande_do_sul.png'),
-    Estado(21,'Rondônia','/static/img/bandeiras/rondonia.png'),
-    Estado(22,'Roraima','/static/img/bandeiras/roraima.png'),
-    Estado(23,'Santa Catarina','/static/img/bandeiras/santa_catarina.png'),
-    Estado(24,'São Paulo','/static/img/bandeiras/sao_paulo.png'),
-    Estado(25,'Sergipe','/static/img/bandeiras/sergipe.png'),
-    Estado(26,'Tocantins','/static/img/bandeiras/tocantins.png')
+    Estado(7,'Espírito Santo','/static/img/bandeiras/Espirito_santo.png'),
+    Estado(8,'Goiás','/static/img/bandeiras/Goias.png'),
+    Estado(9,'Maranhão','/static/img/bandeiras/Maranhão.png'),
+    Estado(10,'Mato Grosso','/static/img/bandeiras/Mato_grosso.png'),
+    Estado(11,'Mato Grosso do Sul','/static/img/bandeiras/Mato_grosso_do_sul.png'),
+    Estado(12,'Minas Gerais','/static/img/bandeiras/Minas_gerais.png'),
+    Estado(13,'Pará','/static/img/bandeiras/Pará.png'),
+    Estado(14,'Paraíba','/static/img/bandeiras/Paraiba.png'),
+    Estado(15,'Paraná','/static/img/bandeiras/Paraná.png'),
+    Estado(16,'Pernambuco','/static/img/bandeiras/Pernambuco.png'),
+    Estado(17,'Piauí','/static/img/bandeiras/Piaui.png'),
+    Estado(18,'Rio de Janeiro','/static/img/bandeiras/Rio_de_janeiro.png'),
+    Estado(19,'Rio Grande do Norte','/static/img/bandeiras/Rio_grande_do_norte.png'),
+    Estado(20,'Rio Grande do Sul','/static/img/bandeiras/Rio_grande_do_sul.png'),
+    Estado(21,'Rondônia','/static/img/bandeiras/Rondonia.png'),
+    Estado(22,'Roraima','/static/img/bandeiras/Roraima.png'),
+    Estado(23,'Santa Catarina','/static/img/bandeiras/Santa_catarina.png'),
+    Estado(24,'São Paulo','/static/img/bandeiras/Sao_paulo.png'),
+    Estado(25,'Sergipe','/static/img/bandeiras/Sergipe.png'),
+    Estado(26,'Tocantins','/static/img/bandeiras/Tocantins.png')
+]
+lista_de_noticia = [
+    Noticia(6,'Rio Branco e outras cidades do Baixo, Alto Acre e Purus avançam para bandeira verde em avaliação do Comitê Covid','https://s2.glbimg.com/Nb5ECEQh2dJEujNB0A8rpjPZMLg=/0x0:540x405/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/u/6/km22dOTRqekbmjdKBptA/capturar.jpg','As regionais do Baixo Acre, Alto Acre e Purus avançaram para a bandeira verde, que representa cuidado, na avaliação do Comitê de Acompanhamento Especial da Covid-19. Já as regionais Juruá e Tarauacá/Envira permaneceram em bandeira amarela, que é de atenção. Os dados foram divulgados nesta sexta-feira (1º) pelo governo do Acre e representa o período de avaliação do dia 13 a 26 de março. Na última avaliação, divulgada no dia 7 de março, o governador Gladson Cameli manteve todas as cidades do Acre em nível de atenção (bandeira amarela) na classificação de risco da pandemia até o dia 31 de março, após aumento de casos no mês de fevereiro.')
 ]
 @app.route('/')
 def home():
-    return render_template('index.html',paginas=listas_de_paginas_principais,paginas_2=listas_de_paginas_recentes,paginas_3=listas_de_paginas_estado,paginas_4 = listas_de_paginas_noticias)
+    return render_template('index.html',paginas=listas_de_paginas_principais,paginas_2=listas_de_paginas_recentes,paginas_3=listas_de_paginas_estado, pagina_4=lista_de_noticia)
 
 @app.route('/manutencao/')
 def manutencao():
@@ -65,10 +67,13 @@ def Principais(id):
 def Recentes(id):
     return render_template('Recentes.html',recentes=listas_de_paginas_recentes[int(id)])
 
-
 @app.route('/estado/<id>')
 def Estados(id):
-    return render_template('Noticia_estado.html',estado=listas_de_paginas_estado[int(id)])
+    return render_template('Estado.html',noticia=lista_de_noticia[int(id)], paginas_3=listas_de_paginas_estado)
+
+@app.route('/noticia/<id>')
+def noticia(id):
+    return render_template('Noticia.html',noticia=listas_de_paginas_principais[int(id)])
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ noticia.py
@@ -0,0 +1,20 @@
+class Noticia:
+
+    def __init__(self,id,titulo,imagem,conteudo):
+        self.__id = id
+        self.__titulo = titulo
+        self.__imagem = imagem
+        self.__conteudo = conteudo
+
+    def get_id(self):
+        return self.__id
+
+    def get_titulo(self):
+        return self.__titulo
+
+    def get_imagem(self):
+        return self.__imagem
+
+    def get_conteudo(self):
+        return self.__conteudo
+
